3123f8d13bb2a18146a92d9582a198c9a721d09b
Liyan <2417357623@qq.com>
2022-09-16 01:14:23 +0800

    9.16 1. 通过修改依赖jar包，和servlet代码页导入的jar包（从jakarta - javax）,实现servlet注解 2. ajax入门

--- demo1/src/main/java/com/learn/AjaxServlet.java
+++ demo1/src/main/java/com/learn/AjaxServlet.java
@@ -1,39 +1,27 @@
-package com.javalearning.item.web;
+package com.learn;
 
-
-import com.javalearning.item.pojo.Brand;
-import com.javalearning.item.service.BrandService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet("/ItemServlet01")
-public class ItemServlet01 extends HttpServlet {
 
-    BrandService brandService = new BrandService();
+
+@WebServlet("/ajaxServlet")
+public class AjaxServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        List<Brand> brands = brandService.selectAll();
-
-        req.setAttribute("brands",brands);
-        req.getRequestDispatcher("/item01.jsp").forward(req,resp);
-
-
-
+        resp.getWriter().write("ajax");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
     }
-
-
 
 }
